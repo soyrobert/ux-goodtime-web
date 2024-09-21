b0f9ec3e0ea878430415df02e6fc305c81beaf4b
Robert Castro <ra.castro2@uniandes.edu.co>
2024-09-21 16:58:16 -0500

    Pantalla listado de alarmas

---
+++ src/app/editar/editar.component.ts
@@ -0,0 +1,93 @@
+import { Component, inject } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './editar.component.html',
+  styleUrl: './editar.component.scss',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    FormsModule
+  ]
+})
+export class EditarComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+imageUrl: any;
+alarmForm: any;
+onFileSelected($event: Event) {
+throw new Error('Method not implemented.');
+}
+saveAlarm() {
+throw new Error('Method not implemented.');
+}
+goBack() {
+throw new Error('Method not implemented.');
+}
+selectImage() {
+throw new Error('Method not implemented.');
+}
+  private breakpointObserver = inject(BreakpointObserver);
+
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 1', cols: 1, rows: 1 },
+          { title: 'Card 2', cols: 1, rows: 1 },
+          { title: 'Card 3', cols: 1, rows: 1 },
+          { title: 'Card 4', cols: 1, rows: 1 }
+        ];
+      }
+
+      return [
+        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 2', cols: 1, rows: 1 },
+        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Card 4', cols: 1, rows: 1 }
+      ];
+    })
+  );
+hour: any;
+minute: any;
+timeFormat: any;
+alarmName: any;
+days: { lu: boolean, ma: boolean, mi: boolean, ju: boolean, vi: boolean, sa: boolean, do: boolean } = {
+  lu: false,
+  ma: false,
+  mi: false,
+  ju: false,
+  vi: false,
+  sa: false,
+  do: false
+};
+
+selectedImageUrl: any;
+description: any;
+}
--- src/app/welcome/welcome-routing.module.ts
+++ src/app/welcome/welcome-routing.module.ts
@@ -1,17 +1,17 @@
-// welcome-routing.module.ts
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { WelcomeComponent } from './welcome.component';
+// // welcome-routing.module.ts
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { WelcomeComponent } from './welcome.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: WelcomeComponent,
-  },
-];
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: WelcomeComponent,
+//   },
+// ];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class WelcomeRoutingModule {}
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule],
+// })
+// export class WelcomeRoutingModule {}
